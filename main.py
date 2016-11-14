031c8eb8cee062c508faaadce81f599520246568
Patrick Vares <p.vares@elsevier.com>
2016-11-14 14:03:15 -0500

    added removal of duplicates

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     outputWs = output.create_sheet('combined',0)
     first = True
     rowTotal = 0
+    writtenRows = []
     for fileName in sys.argv[1:]:
         if(not os.path.isfile(fileName)):
             print('Cannot find the file "{}". Check the name and try again'.format(fileName))
@@ -19,18 +20,25 @@ def main():
         wb = Workbook()
         wb = load_workbook(filename=fileName, read_only=True)
         skip=True
+        skipped = 0
         for rowNum,r in enumerate(wb.active.rows, 1):
             if(skip and not first):
                 skip=False
+                skipped += 1
                 continue
             skip=False
-            for i, cell in enumerate(r, 1):
-                outputWs.cell(row=rowNum+rowTotal, column=i).value = cell.value
-                # print('output[{}:{}] = {}'.format(rowNum,i,outputWs.cell(row=rowNum, column=i).value))
-        rowTotal += rowNum - 1
+            cellsToWrite = []
+            for cell in r:
+                cellsToWrite.append(cell.value)
+            if(cellsToWrite not in writtenRows):
+                for i,value in enumerate(cellsToWrite, 1):
+                    outputWs.cell(row=rowNum+rowTotal - skipped, column=i).value = value
+                writtenRows.append(cellsToWrite)
+            else:
+                skipped += 1
+        rowTotal += rowNum - skipped
         first = False
-        print(output["combined"]["A1"].value)
-        output.save('output.xlsx')
+    output.save('output.xlsx')
 
 if __name__ == '__main__':
     main()
